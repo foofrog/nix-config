975ac6943963b3d4bbf4611811eff753f74e6629
Foo the Frog <mountfooji@proton.me>
2024-06-11 22:45:12 +0600

    Enable per-user config with `home-manager.users` key

--- modules/users/foo/home-depot/programs.nix
+++ modules/users/foo/home-depot/programs.nix
@@ -2,64 +2,66 @@
 { pkgs, ... }:
 
 {
+  
+  home-manager.users.foo = {
+    gtk = {
+      enable = true;
 
-  gtk = {
-    enable = true;
-
-    cursorTheme = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
+      cursorTheme = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Ice";
+      };
     };
-  };
 
-  qt = {
-    enable = true;
+    qt = {
+      enable = true;
 
-    platformTheme.name = "gtk";
-    style = {
-      package = pkgs.adwaita-qt;
+      platformTheme.name = "gtk";
+      style = {
+        package = pkgs.adwaita-qt;
 
-      /*
-      Detects the following automatically:
-      adwaita, adwaita-dark, adwaita-highcontrast,
-      adwaita-highcontrastinverse, breeze,
-      bb10bright, bb10dark, cde, cleanlooks,
-      gtk2, motif, plastique
-      */
-      name = "adwaita-dark";
+        /*
+        Detects the following automatically:
+        adwaita, adwaita-dark, adwaita-highcontrast,
+        adwaita-highcontrastinverse, breeze,
+        bb10bright, bb10dark, cde, cleanlooks,
+        gtk2, motif, plastique
+        */
+        name = "adwaita-dark";
+      };
     };
-  };
 
-  programs.fish = {
-    enable = true;
-    shellAliases = {
-      ".." = "cd ..";
-      "..." = "cd ../..";
-      "...." = "cd ../../..";
-      "....." = "cd ../../../..";
+    programs.fish = {
+      enable = true;
+      shellAliases = {
+        ".." = "cd ..";
+        "..." = "cd ../..";
+        "...." = "cd ../../..";
+        "....." = "cd ../../../..";
 
-      "ls" = "eza -al --color=always --group-directories-first";
-      "cat" = "bat --style header --style snip --style changes --style header";
+        "ls" = "eza -al --color=always --group-directories-first";
+        "cat" = "bat --style header --style snip --style changes --style header";
 
-      "jctl" = "journalctl -p 3 -xb";
+        "jctl" = "journalctl -p 3 -xb";
+      };
+      interactiveShellInit = ''
+        set fish_greeting
+      '';
     };
-    interactiveShellInit = ''
-      set fish_greeting
-    '';
-  };
 
-  programs.starship = {
-    enable = true;
-    enableFishIntegration = true;
-  };
+    programs.starship = {
+      enable = true;
+      enableFishIntegration = true;
+    };
 
-  programs.git = {
-    enable = true;
-    userName = "Foo the Frog";
-    userEmail = "mountfooji@proton.me";
-  };
+    programs.git = {
+      enable = true;
+      userName = "Foo the Frog";
+      userEmail = "mountfooji@proton.me";
+    };
 
-  # Let `home-manager` install and manage itself
-  programs.home-manager.enable = true;
+    # Let `home-manager` install and manage itself
+    programs.home-manager.enable = true;
+  };
 
 }
