86c58881c5bdd021fbdb93500e29e650db56dcec
Foo the Frog <mountfooji@proton.me>
2024-06-11 23:12:10 +0600

    Disable `editor` for `systemd-boot`

--- modules/bootloader.nix
+++ modules/bootloader.nix
@@ -5,7 +5,15 @@
   boot.loader.systemd-boot = {
     # Limit the number of generations that show up at boot entries
     configurationLimit = lib.mkDefault 5;
-    
+
+      # Enable editing the kernel command-line before boot
+      editor = false; # is enabled (by default) unless set to false
+      /*
+      Here,
+      It is recommended to set `editor` to false
+      Otherwise it allows to gain root access by passing `init=/bin/sh` as kernel parameter
+      */
+
     # Set the highest resolution for `sytemd-boot`'s console
     consoleMode = lib.mkDefault "max";
   };
